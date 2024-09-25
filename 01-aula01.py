2e4ad754631957faf709fde479ab28163fabe00d
eduhernandes2f <eduhernandes2f@firemail.com.br>
2024-09-25 14:08:01 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,20 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Eduardo")  # comentário a linha do código
+print('Eduardo')
+print('Eduardo "Dudu" Hernandes')
+print("Eduardo 'Dudu' Hernandes")
+print("Eduardo \"Dudu\" Hernandes")
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
